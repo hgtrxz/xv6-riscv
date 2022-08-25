dc70e3a189ddb71e8304fe7d6dd686202f2e49c1
hgtrxz <3043719361@qq.com>
2022-08-25 23:23:01 +0800

    define sleep.c

---
+++ user/hello.c
@@ -0,0 +1,7 @@
+//
+// Created by Shize Wang on 2022/8/25.
+//
+
+int main() {
+
+}
---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+//
+// Created by Shize Wang on 2022/8/25.
+//
+// 1.Look at some other programs in user/ (e.g., user/echo.c, user/grep.c, and user/rm.c)
+//   to see how you can obtain the command-line arguments passed to a program.
+//
+// 2.The command-line argument is passed as a string; you can convert it to an integer using atoi (see user/ulib.c).
+//
+// 3.See kernel/sysproc.c for the xv6 kernel code that implements the sleep system call (look for sys_sleep),
+//   user/user.h for the C definition of sleep callable from a user program,
+//   and user/usys.S for the assembler code that jumps from user code into the kernel for sleep.
+//
+// 4.Make sure main calls exit() in order to exit your program.
+//
+// 5.Add your sleep program to UPROGS in Makefile; once you've done that,
+//   make qemu will compile your program, and you'll be able to run it from the xv6 shell.
+//
+
